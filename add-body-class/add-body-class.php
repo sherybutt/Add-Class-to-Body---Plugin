9e7a9e187b9fa72a62f1add9fd9091b19b5c2f5b
Sheryar Butt <73537344+sherybutt@users.noreply.github.com>
2024-05-11 18:20:20 +0500

    Update add-body-class.php

--- add-body-class/add-body-class.php
+++ add-body-class/add-body-class.php
@@ -3,7 +3,7 @@
 Plugin Name: Add Class to Body
 Description: Add desired class to body.
 Version: 1.2
-Author: Sheryar
+Author: Sheryar Shahzad
 */
 
 // Exit if accessed directly.
@@ -12,44 +12,44 @@ if (!defined('ABSPATH')) {
 }
 
 // Activation Hook
-register_activation_hook(__FILE__, 'your_plugin_activate');
+register_activation_hook(__FILE__, 'ssbwp_plugin_activate');
 
-function your_plugin_activate()
+function ssbwp_plugin_activate()
 {
     // Activation code, if needed
 }
 
 // Function to add the new option in WordPress settings
-function custom_options_add_option()
+function ssbwp_custom_options_add_option()
 {
     add_options_page(
         'Custom Options',      // Page title
         'Add Class to Body',   // Menu title
         'manage_options',      // Capability required to access
-        'custom_options_page', // Page slug
-        'custom_options_page'  // Callback function to render the page
+        'ssbwp_custom_options_page', // Page slug
+        'ssbwp_custom_options_page'  // Callback function to render the page
     );
 }
 
 // Callback function to render the custom options page
-function custom_options_page()
+function ssbwp_custom_options_page()
 {
     $success_message = '';
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['save_settings'])) {
         $selected_post_types = isset($_POST['post_types']) ? $_POST['post_types'] : array();
-        update_option('hidden_post_types', $selected_post_types);
+        update_option('ssbwp_hidden_post_types', $selected_post_types);
 
         $global_class = sanitize_text_field($_POST['global_class']);
-        update_option('global_class', $global_class);
+        update_option('ssbwp_global_class', $global_class);
 
         $success_message = 'Changes saved successfully!';
     }
 
-    $hidden_post_types = get_option('hidden_post_types', array());
+    $hidden_post_types = get_option('ssbwp_hidden_post_types', array());
     $all_post_types = get_post_types(array('public' => true, '_builtin' => false), 'names');
     $registered_post_types = array_merge(array('post', 'page'), $all_post_types);
-    $global_class = get_option('global_class', '');
+    $global_class = get_option('ssbwp_global_class', '');
 
     ?>
     <div class="wrap">
@@ -82,37 +82,37 @@ function custom_options_page()
 }
 
 // Hook to add the custom option in the WordPress settings menu
-add_action('admin_menu', 'custom_options_add_option');
+add_action('admin_menu', 'ssbwp_custom_options_add_option');
 
 // Enqueue styles for the admin dashboard
-function enqueue_dashboard_styles()
+function ssbwp_enqueue_dashboard_styles()
 {
     // Adjust the path to your stylesheet file if needed
     $stylesheet_url = plugin_dir_url(__FILE__) . 'admin/css/admin-styles.css';
 
     // Enqueue the stylesheet only on the WordPress admin pages
-    wp_enqueue_style('dashboard-styles-css', $stylesheet_url);
+    wp_enqueue_style('ssbwp-dashboard-styles-css', $stylesheet_url);
 }
 
-add_action('admin_enqueue_scripts', 'enqueue_dashboard_styles');
+add_action('admin_enqueue_scripts', 'ssbwp_enqueue_dashboard_styles');
 
 // Add meta box to posts, pages, and custom post types
-function add_custom_meta_box()
+function ssbwp_add_custom_meta_box()
 {
-    $hidden_post_types = get_option('hidden_post_types', array());
+    $hidden_post_types = get_option('ssbwp_hidden_post_types', array());
     $post_types = array_diff(get_post_types(), $hidden_post_types);
 
     foreach ($post_types as $post_type) {
-        add_meta_box('custom_class_meta_box', 'Custom Classes', 'render_custom_class_meta_box', $post_type, 'side', 'default');
+        add_meta_box('ssbwp_custom_class_meta_box', 'Custom Classes', 'ssbwp_render_custom_class_meta_box', $post_type, 'side', 'default');
     }
 }
 
-add_action('add_meta_boxes', 'add_custom_meta_box');
+add_action('add_meta_boxes', 'ssbwp_add_custom_meta_box');
 
 // Render the meta box content
-function render_custom_class_meta_box($post)
+function ssbwp_render_custom_class_meta_box($post)
 {
-    $custom_classes = get_post_meta($post->ID, '_custom_classes', true);
+    $custom_classes = get_post_meta($post->ID, '_ssbwp_custom_classes', true);
     ?>
     <label for="custom_classes">Enter custom classes <br />(Add comma-separator or space for multiple classes):</label><br /><br />
     <input type="text" class="wpmd-custom-input" id="custom_classes" name="custom_classes" value="<?php echo esc_attr($custom_classes); ?>" style="width: 100%;">
@@ -120,9 +120,9 @@ function render_custom_class_meta_box($post)
 }
 
 // Save the custom class meta data
-function save_custom_class_meta_data($post_id)
+function ssbwp_save_custom_class_meta_data($post_id)
 {
-    $hidden_post_types = get_option('hidden_post_types', array());
+    $hidden_post_types = get_option('ssbwp_hidden_post_types', array());
     $current_post_type = get_post_type($post_id);
 
     if (!in_array($current_post_type, $hidden_post_types)) {
@@ -140,32 +140,32 @@ function save_custom_class_meta_data($post_id)
 
                 // Update post meta only if all class names are valid
                 if (count($class_names) === count($valid_class_names)) {
-                    update_post_meta($post_id, '_custom_classes', $custom_classes);
+                    update_post_meta($post_id, '_ssbwp_custom_classes', $custom_classes);
                 }
             } else {
                 // Remove the custom class meta data if the input field is empty
-                delete_post_meta($post_id, '_custom_classes');
+                delete_post_meta($post_id, '_ssbwp_custom_classes');
             }
         }
     }
 }
 
-add_action('save_post', 'save_custom_class_meta_data');
+add_action('save_post', 'ssbwp_save_custom_class_meta_data');
 
 // Add custom classes to the body tag
-function add_custom_classes_to_body($classes)
+function ssbwp_add_custom_classes_to_body($classes)
 {
-    $global_class = get_option('global_class', '');
+    $global_class = get_option('ssbwp_global_class', '');
 
     if ($global_class) {
         $classes[] = sanitize_html_class($global_class);
     }
 
     global $post;
-    $hidden_post_types = get_option('hidden_post_types', array());
+    $hidden_post_types = get_option('ssbwp_hidden_post_types', array());
 
     if (!in_array($post->post_type, $hidden_post_types)) {
-        $custom_classes = get_post_meta($post->ID, '_custom_classes', true);
+        $custom_classes = get_post_meta($post->ID, '_ssbwp_custom_classes', true);
 
         if ($custom_classes) {
             $custom_classes_array = explode(',', $custom_classes);
@@ -176,5 +176,5 @@ function add_custom_classes_to_body($classes)
     return $classes;
 }
 
-add_filter('body_class', 'add_custom_classes_to_body');
+add_filter('body_class', 'ssbwp_add_custom_classes_to_body');
 ?>
